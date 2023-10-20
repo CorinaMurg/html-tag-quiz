1f18e97088746e8c0e53d07faf0f9d141eb22e1c
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-10-19 23:11:49 -0400

    feature-25[Add aria-label and appropriate JS logic]

--- src/progressBar.js
+++ src/progressBar.js
@@ -10,6 +10,26 @@ export function updateProgressBar() {
 
     // FOR SCREEN READERS
 
+    // NOT HAVING SUCCESS WITH ARIA-LABEL. I ASSUMED WHEN THE SCREEN READER SCANS THE PAGE, 
+    // IT WOULD READ THE ARIA-LABEL ATTRIBUTE, BUT IT DOES NOT.
+    // ARIA-LABEL SIMPLIFIED VERSION
+    let ariaLabel = `Your progress: ${tagsCurrentCount} out of ${tagsMaxCount} tags added.`;
+    progressBar.setAttribute('aria-label', ariaLabel);
+
+    // ARIA-LABEL DETAILED VERSION: WITH PERCENTAGE ANNOUNCEMENT AT 50% AND 100% COMPLETION
+    // let percentageMessage = '';  
+
+    // if (tagsCurrentCount === 62) {
+    //     percentageMessage = 'This is 50% completion. ';  
+    // } else if (tagsCurrentCount === tagsMaxCount) {
+    //     percentageMessage = 'This is 100% completion. ';  
+    // }
+
+    // let ariaLabel = `Your progress: ${tagsCurrentCount} out of ${tagsMaxCount} tags added. ${percentageMessage}`;
+    // progressBar.setAttribute('aria-label', ariaLabel);
+    // END OF ARIA-LABEL
+
+
     // MDN DOCS RECOMMEND USING IT, BUT I DID NOT NOTICE ANY DIFFERENCE WHEN SCREEN READER WAS ON
     let ariaValueText = `${tagsCurrentCount}`;
     progressBar.setAttribute('aria-valuetext', ariaValueText);
@@ -47,4 +67,4 @@ export function updateProgressBar() {
     }
 }
 
-
+// THE CODE IS NOT DRY. IT SHOULD BE DONE ONCE WE DECIDE WHICH ATTRIBUTES TO USE AND HOW TO USE THEM.
