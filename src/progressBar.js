a442a077a2271b2869d189fa77df4905cc26453f
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-10-21 22:12:38 -0400

    feature-25[Replace aria attributes from <progress> with min and max]

--- src/progressBar.js
+++ src/progressBar.js
@@ -1,6 +1,7 @@
+import { tagsW3 } from "./tags.js";
 
 export function updateProgressBar() {
-    let tagsMaxCount = 124;
+    let tagsMaxCount = tagsW3.length;
     let tagsCurrentCount = document.getElementById("list").querySelectorAll("li").length - 1;  // Subtracting 1 because of the "Inputs" list item
     
     let progressPercentage = (tagsCurrentCount / tagsMaxCount) * 100;
@@ -14,29 +15,22 @@ export function updateProgressBar() {
     let progressDescription = document.getElementById('progress-description')
     progressDescription.textContent = `Your progress: ${tagsCurrentCount} out of ${tagsMaxCount} tags added.`;
 
-    // MDN DOCS RECOMMEND USING IT, BUT I DID NOT NOTICE ANY DIFFERENCE WHEN SCREEN READER WAS ON.
-    // NVDA only announces the label, while the Narrator announces the label (twice) and the %, but 
-    // this is regardless whether aria-valuetext is used or not.
-    let ariaValueText = `${tagsCurrentCount}`;
-    progressBar.setAttribute('aria-valuetext', ariaValueText);
-
-    // this will announce the current count after each input. TOO MUCH? MAKE IT EVERY 10 INPUTS?
-    // OR NOT AT ALL? JUST UPDATE AT 50% AND 100%?
+    // this will announce the current count after each input. 
     let ariaMessage = `${tagsCurrentCount} added.`;
     let progressLive = document.getElementById('progress-live');
     progressLive.textContent = ariaMessage;
 
     // TEST CASES for updating aria-live region. Note: the values are made up.
-    if (tagsCurrentCount === 5) {
-        let ariaMessage = `Your progress: ${tagsCurrentCount} added. This is 5%. Great job.`;
-        let progressLive = document.getElementById('progress-live');
-        progressLive.textContent = ariaMessage;
-    } 
-    if (tagsCurrentCount === 10) {
-        let ariaMessage = `Your progress: ${tagsCurrentCount} added. This is 10%. Great job.`;
-        let progressLive = document.getElementById('progress-live');
-        progressLive.textContent = ariaMessage;
-    }
+    // if (tagsCurrentCount === 5) {
+    //     let ariaMessage = `Your progress: ${tagsCurrentCount} added. This is 5%. Great job.`;
+    //     let progressLive = document.getElementById('progress-live');
+    //     progressLive.textContent = ariaMessage;
+    // } 
+    // if (tagsCurrentCount === 10) {
+    //     let ariaMessage = `Your progress: ${tagsCurrentCount} added. This is 10%. Great job.`;
+    //     let progressLive = document.getElementById('progress-live');
+    //     progressLive.textContent = ariaMessage;
+    // }
     // END OF TEST CASES
 
     // Update aria-live region when progress reaches half or full completion
@@ -53,4 +47,3 @@ export function updateProgressBar() {
     }
 }
 
-// THE CODE IS NOT DRY. IT SHOULD BE DONE ONCE WE DECIDE WHICH ATTRIBUTES TO USE AND HOW TO USE THEM.
